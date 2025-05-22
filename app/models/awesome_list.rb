5991da712557065a8b8e6ab98c8570a49abd06eb
Patrick Clery <patrick@blacbook.tiger-alligator.ts.net>
2025-05-21 23:14:25 -0400

    Update associations in models to reference awesome_list instead of awesome_list_version

--- app/models/awesome_list.rb
+++ app/models/awesome_list.rb
@@ -4,12 +4,13 @@
 #
 # Table name: awesome_lists
 #
-#  id          :integer          not null, primary key
-#  description :text
-#  github_repo :string           not null
-#  name        :string           not null
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
+#  id             :integer          not null, primary key
+#  description    :text
+#  github_repo    :string           not null
+#  last_commit_at :datetime
+#  name           :string           not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
 #
 class AwesomeList < ApplicationRecord
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -4,23 +4,23 @@
 #
 # Table name: categories
 #
-#  id                      :integer          not null, primary key
-#  name                    :string           not null
-#  repo_count              :integer
-#  created_at              :datetime         not null
-#  updated_at              :datetime         not null
-#  awesome_list_version_id :integer          not null
-#  parent_id               :integer
+#  id              :integer          not null, primary key
+#  name            :string           not null
+#  repo_count      :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  awesome_list_id :integer          not null
+#  parent_id       :integer
 #
 # Indexes
 #
-#  index_categories_on_awesome_list_version_id  (awesome_list_version_id)
-#  index_categories_on_parent_id                (parent_id)
+#  index_categories_on_awesome_list_id  (awesome_list_id)
+#  index_categories_on_parent_id        (parent_id)
 #
 # Foreign Keys
 #
-#  awesome_list_version_id  (awesome_list_version_id => awesome_list_versions.id)
-#  parent_id                (parent_id => categories.id)
+#  awesome_list_id  (awesome_list_id => awesome_lists.id)
+#  parent_id        (parent_id => categories.id)
 #
 class Category < ApplicationRecord
   belongs_to :awesome_list_version
